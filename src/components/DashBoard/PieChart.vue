312adec2525bf0c051c8dfc5b36d474b816cc8c5
Benoit.Spriet <benoit.spriet@epitech.eu>
2021-08-12 13:12:04 +0200

    bar en cours

--- src/components/DashBoard/PieChart.vue
+++ src/components/DashBoard/PieChart.vue
@@ -1,33 +1,23 @@
 <script>
-import { Pie } from 'vue-chartjs'
+import { Pie } from "vue-chartjs";
+import { mapState, mapActions } from "vuex";
+import freetoplay from "@/common/api-freetoplay";
 
 export default {
   extends: Pie,
   data() {
     return {
       chartData: {
-        labels: ['Rebel INC', 'Animal Crossing', 'Once Upon A Tower', 'Fortnite','Clash Royale'],
-        datasets: [
-          {
-            borderWidth: 1,
-            borderColor: [
-              'rgba(255,99,132,1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              '#EC7394',
-            ],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              '#F9ACC1'
-            ],
-            data: [1000, 500, 1500, 1000, 800],
-          },
-        ],
-      },
+      labels: ["Brawlhalla", "Steel Legions", "Survivor Legacy", "Allods Online", "Phantasy Star Online 2 New Genesis"],
+      datasets: [
+        {
+          borderWidth: 1,
+          borderColor: ["rgba(255,99,132,1)", "rgba(54, 162, 235, 1)", "rgba(255, 206, 86, 1)", "rgba(75, 192, 192, 1)", "#EC7394"],
+          backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)", "rgba(255, 206, 86, 0.2)", "rgba(75, 192, 192, 0.2)", "#F9ACC1"],
+          data: ["2", "2", "2", "2", "1"],
+        },
+      ],
+    },
       options: {
         legend: {
           display: true,
@@ -35,10 +25,58 @@ export default {
         responsive: true,
         maintainAspectRatio: false,
       },
-    }
+    };
   },
-  mounted() {
-    this.renderChart(this.chartData, this.options)
+  computed: {
+    ...mapState("authentification", {
+      token: (state) => state.user.token,
+    }),
+    ...mapState("dashboard", {
+      gamesPopularity: (state) => state.gamesPopularity,
+    }),
+    ...mapState("games", {
+      game: (state) => state.game,
+    }),
   },
-}
+  methods: {
+    ...mapActions({
+      fetchGamesPopularity: "dashboard/FETCH_GAMES_POPULARITY",
+      fetchGame: "games/GET_GAME",
+    }),
+    async FETCH_GAME(id) {
+      const { data } = await freetoplay
+        .get("/game/", {
+          params: {
+            id: id,
+          },
+        })
+        .catch((error) => console.log(JSON.stringify(error.message)));
+      return data.title;
+    },
+  },
+  async mounted() {
+    let labels = [];
+    let data = [];
+    this.fetchGamesPopularity(this.token);
+    this.gamesPopularity.sum.forEach(async (game) => {
+      let title = await this.FETCH_GAME(game.game_id);
+      labels.push(title);
+      data.push(parseInt(game.sum, 10));
+    });
+    let chartData = {
+      labels: labels,
+      datasets: [
+        {
+          borderWidth: 1,
+          borderColor: ["rgba(255,99,132,1)", "rgba(54, 162, 235, 1)", "rgba(255, 206, 86, 1)", "rgba(75, 192, 192, 1)", "#EC7394"],
+          backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)", "rgba(255, 206, 86, 0.2)", "rgba(75, 192, 192, 0.2)", "#F9ACC1"],
+          data: data,
+        },
+      ],
+    };
+    console.log(chartData.datasets[0].data);
+    console.log(chartData.labels);
+    this.renderChart(this.chartData, this.options);
+  },
+};
 </script>
--- src/store/modules/dashboard.js
+++ src/store/modules/dashboard.js
@@ -7,9 +7,14 @@ export default {
         "labels": [],
         "data": [],
     },
+    gamesPopularity: {
+        "count": [],
+        "sum": [],
+    },
   },
   mutations: {
     SET_VISITS: (state, data) => ((state.visits.labels = data.labels), (state.visits.data = data.data)),
+    SET_GAMES_POPULARITY: (state, data) => ((state.gamesPopularity.count = data.count), (state.gamesPopularity.sum = data.sum)),
   },
   actions: {
     async FETCH_VISITS({ commit }, token) {
@@ -20,6 +25,14 @@ export default {
       const { data } = await apiLaravel.get("/count/visits/week", { headers: headers }).catch((error) => console.log(JSON.stringify(error.message)));
       commit("SET_VISITS", data.data);
     },
+    async FETCH_GAMES_POPULARITY({ commit }, token) {
+        let headers = {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        };
+        const { data } = await apiLaravel.get("/count/games/5", { headers: headers }).catch((error) => console.log(JSON.stringify(error.message)));
+        commit("SET_GAMES_POPULARITY", data.data);
+      },
   },
   getters: {
     GET_USER: (state) => {
