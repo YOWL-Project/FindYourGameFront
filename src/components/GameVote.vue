f4a6871c54a1dcb22971029709da75b861d834c1
ManonVioleau <manonvioleau34@outlook.fr>
2021-08-12 11:06:39 +0200

    votesGames

--- src/components/GameVote.vue
+++ src/components/GameVote.vue
@@ -3,19 +3,31 @@
     <div class="row">
       <h2>VOTE FOR THIS GAME !</h2>
     </div>
+    <div
+      v-if="message != ''"
+      style="
+        background-color: rgb(255, 0, 0, 0.1);
+        color: rgb(255, 255, 255, 0.7);
+        border: 1px solid rgb(255, 255, 255, 0.3);
+      "
+    >
+      {{ message }}
+    </div>
     <div class="row">
       <div class="col-6" align="center">
         <img
           src="../assets/positif.svg"
           width="65"
           height="65"
-          class="positif"
+          class="pouce positif"
+          @click="voteplus(game.id)"
         />
         <img
           src="../assets/negatif.svg"
           width="65"
           height="65"
-          class="negatif"
+          class="pouce negatif"
+          @click="voteminus(game.id)"
         />
       </div>
       <div class="col-6 d-flex" align="left">
@@ -36,32 +48,36 @@
         <p class="nb-votes">{{ getNbVotesGames(game.id).nb_votes }} votes</p>
       </div>
     </div>
-    <div
+    <!-- <div
       class="row"
       id="lastvotes"
       v-if="getNbVotesGames(game.id).nb_votes > 0"
     >
       <h2>LAST VOTES</h2>
-      <div class="row" v-for="votesGame in votesGames" :key="votesGame.id" :votesGame="votesGame">
+      <div
+        class="row"
+        v-for="votesGame in votesGames"
+        :key="votesGame.id"
+        :votesGame="votesGame"
+      >
         <div class="col-12" v-if="votesGame.game_id == game.id">
-        <img
-          src="../assets/negatif.svg"
-          width="45"
-          height="45"
-          class="votes"
-          v-if="votesGame.vote == -1"
-        />
-        <img
-          src="../assets/positif.svg"
-          width="45"
-          height="45"
-          class="votes"
-          v-if="votesGame.vote == 1"
-        />
-
+          <img
+            src="../assets/negatif.svg"
+            width="45"
+            height="45"
+            class="votes"
+            v-if="votesGame.vote == -1"
+          />
+          <img
+            src="../assets/positif.svg"
+            width="45"
+            height="45"
+            class="votes"
+            v-if="votesGame.vote == 1"
+          />
         </div>
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -76,15 +92,22 @@ export default {
 
   data: () => ({
     readMore: false,
+    message: "",
   }),
   computed: {
     ...mapState("votesGames", {
       votesGames: (state) => state.votesGames,
     }),
+    ...mapState("authentification", {
+      user: (state) => state.user,
+      authentificated: (state) => state.authentificated,
+    }),
   },
   methods: {
     ...mapActions({
       fetchVotesGames: "votesGames/FETCH_VOTESGAMES",
+      createVotesGames: "votesGames/CREATE_VOTESGAMES",
+      updateVotesGames: "votesGames/UPDATE_VOTESGAMES",
     }),
     getNbVotesGames(game_id) {
       let nb_votes = 0;
@@ -105,9 +128,110 @@ export default {
       let result = { nb_votes: nb_votes, tendance: tendance };
       return result;
     },
+    voteplus() {
+      let infos = {
+        body: {
+          game_id: this.game.id,
+          user_id: this.user.id,
+          vote: 1,
+        },
+        headers: {
+          Authorization: `Bearer ${this.user.token}`,
+        },
+      };
+      let alreadyVote = [];
+      if (this.authentificated) {
+        this.votesGames.forEach((votesGame) => {
+          if (
+            votesGame.game_id == this.game.id &&
+            votesGame.user_id == this.user.id
+          ) {
+            alreadyVote.push(votesGame.id);
+          }
+        });
+        if (alreadyVote) {
+          console.log(infos);
+          this.message = "";
+          this.createVotesGames(infos);
+        } else {
+          infos = {
+            id: alreadyVote[0],
+            body: {
+              game_id: this.game.id,
+              user_id: this.user.id,
+              vote: 1,
+            },
+            headers: {
+              Authorization: `Bearer ${this.user.token}`,
+            },
+          };
+          console.log(alreadyVote);
+          this.updateVotesGames(infos);
+        }
+      } else {
+        this.message = "You have to be authentificated to vote";
+      }
+    },
+    voteminus() {
+      let infos = {
+        body: {
+          game_id: this.game.id,
+          user_id: this.user.id,
+          vote: -1,
+        },
+        headers: {
+          Authorization: `Bearer ${this.user.token}`,
+        },
+      };
+      let alreadyVote = [];
+      if (this.authentificated) {
+        this.votesGames.forEach((votesGame) => {
+          if (
+            votesGame.game_id == this.game.id &&
+            votesGame.user_id == this.user.id
+          ) {
+            alreadyVote.push(votesGame.id);
+          }
+        });
+        if (alreadyVote != []) {
+          infos = {
+            id: alreadyVote[0],
+            body: {
+              game_id: this.game.id,
+              user_id: this.user.id,
+              vote: 1,
+            },
+            headers: {
+              Authorization: `Bearer ${this.user.token}`,
+            },
+          };
+          this.updateVotesGames(infos);
+        } else {
+          this.message = "";
+          this.createVotesGames(infos);
+        }
+      } else {
+        this.message = "You have to be authentificated to vote";
+      }
+    },
+    voted() {
+      this.votesGames.forEach((votesGame) => {
+        if (
+          votesGame.game_id == this.game.id &&
+          votesGame.user_id == this.user.id
+        ) {
+          if (votesGame.vote == 1) {
+            document.querySelector(".positif").classList.add("checked");
+          } else {
+            document.querySelector(".negatif").classList.add("checked");
+          }
+        }
+      });
+    },
   },
   mounted() {
     this.fetchVotesGames();
+    this.voted();
   },
 };
 </script>
@@ -144,5 +268,14 @@ h2 {
 
 #lastvotes {
   margin-top: 3%;
+}
+
+.pouce {
+  cursor: pointer;
+  border-radius: 50%;
+}
+
+.checked {
+  background-color: rgb(0, 0, 0, 0.5);
 }
 </style>
--- src/store/modules/votesGames.js
+++ src/store/modules/votesGames.js
@@ -7,12 +7,33 @@ export default {
     },
     mutations: {
         SET_VOTESGAMES: (state, votesGames) => (state.votesGames = votesGames),
+        CREATE_VOTESGAMES: (state, voteGame) => (state.votesGames = state.votesGames.push(voteGame)),
+        DELETE_VOTESGAMES: (state, id) => {
+            let index = state.votesGames.findIndex((elem) => elem.id === id)
+            state.votesGames.splice(index, 1)
+        }
     },
     actions: {
         async FETCH_VOTESGAMES({ commit }) {
             const { data } = await apiLaravel.get('/votesgames/')
                 .catch((error) => console.log(JSON.stringify(error.message)));
             commit("SET_VOTESGAMES", data.data.votes);
+        },
+        async CREATE_VOTESGAMES({ commit }, infos) {
+            const { data } = await apiLaravel.post('/votesgames/', infos.body, { headers: infos.headers })
+                .catch((error) => console.log(JSON.stringify(error.message)));
+            commit("CREATE_VOTESGAMES", data.data);
+        },
+        async DELETE_VOTESGAMES({ commit }, infos) {
+            const { data } = await apiLaravel.delete(`/votesgames/${infos.id}`, { headers: infos.headers })
+                .catch((error) => console.log(JSON.stringify(error.message)));
+            commit("DELETE_VOTESGAMES", data.data.id);
+        },
+        async UPDATE_VOTESGAMES({ commit }, infos) {
+            const { data } = await apiLaravel.put(`/votesgames/${infos.id}`, infos.body, { headers: infos.headers })
+                .catch((error) => console.log(JSON.stringify(error.message)));
+            commit("DELETE_VOTESGAMES", data.data.id);
+            commit("CREATE_VOTESGAMES", data.data.id);
         },
     },
 }
